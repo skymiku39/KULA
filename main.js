9c0b64961e2270295de4cc5d69d9c8e7fdf91c08
skymiku39 <tianya.wakfu@gmail.com>
2019-07-13 16:08:09 +0800

    1.Add var canvasHeight = document.getElementById("myCanvas").height; 2.Add var canvasWidth = document.getElementById("myCanvas").width; 3.新增 邊界碰撞檢查 4.新增 刪除碰到邊界的士兵 5.Change  soldierList; 友軍List: alliesSoldierList 6.Add 敵軍List: enemySoldierList; 7.Change myFun() -> alliesSoldier()

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 //var x = 70;
-var soldierList;
+var alliesSoldierList;
+var enemySoldierList;
+var time = 0;
+var canvasHeight = document.getElementById("myCanvas").height;
+var canvasWidth = document.getElementById("myCanvas").width;
+
+
 
 function Initiail() {
-    soldierList = new Array();
+    alliesSoldierList = new Array();
+    enemySoldierList = new Array();
     run();
 }
 
-function myFun() {
-    let soldier = new Soldier(70);
+function alliesSoldier() {
+    let soldier = new Soldier(70, 700, 50, 80);
     soldier.draw();
 
-    soldierList.push(soldier);
+    alliesSoldierList.push(soldier);
+}
+
+function rightSoldier() {
+    let soldier = new Soldier(1130, 700, 50, 80);
+    soldier.speed = -2;
+    soldier.draw();
+
+    enemySoldierList.push(soldier);
 }
 
 function sleep(ms) {
@@ -19,23 +34,62 @@ function sleep(ms) {
 
 function clearCanvas() {
     var cxt = document.getElementById("myCanvas").getContext("2d");
-    cxt.clearRect(0, 0, 1200, 800);
+    cxt.clearRect(0, 0, canvasWidth, canvasHeight);
 
     // let canvas = document.getElementById("myCanvas");
     // canvas.width = canvas.width;
 }
 
 function processData() {
-    for (let index = 0; index < soldierList.length; index++) {
-        soldierList[index].move();
+    //移動士兵
+    for (let index = 0; index < alliesSoldierList.length; index++) {
+        alliesSoldierList[index].move();
     }
+    for (let index = 0; index < enemySoldierList.length; index++) {
+        enemySoldierList[index].move();
+    }
+    //刪除友軍碰到邊界士兵
+    for (let index = 0; index < alliesSoldierList.length; index++) {
+        //左邊界_友軍
+        if (alliesSoldierList[index].x + alliesSoldierList[index].width < 0) {
+            alliesSoldierList.splice(index, 1)
+        }
+        //右邊界_友軍
+        if (alliesSoldierList[index].x + alliesSoldierList[index].width > canvasWidth) {
+            alliesSoldierList.splice(index, 1)
+        }
+    }
+    //刪除敵軍碰到邊界士兵
+    for (let index = 0; index < enemySoldierList.length; index++) {
+        //左邊界_敵軍
+        if (enemySoldierList[index].x < 0) {
+            enemySoldierList.splice(index, 1)
+        }
+        //右邊界_敵軍
+        if (enemySoldierList[index].x > canvasWidth) {
+            enemySoldierList.splice(index, 1)
+
+        }
+    }
+    //產生敵方士兵
+    if (time % 50 == 0) {
+        rightSoldier();
+    }
+    //短兵相接
+
+
+
+
 }
 
 function updateView() {
     clearCanvas();
 
-    for (let index = 0; index < soldierList.length; index++) {
-        soldierList[index].draw();
+    for (let index = 0; index < alliesSoldierList.length; index++) {
+        alliesSoldierList[index].draw();
+    }
+    for (let index = 0; index < enemySoldierList.length; index++) {
+        enemySoldierList[index].draw();
     }
 }
 
@@ -44,8 +98,8 @@ function run() {
     // checkLimitAndRun(circleX, circleY, circleR, xSpeed, ySpeed);
     processData();
     updateView();
-
-    setTimeout(run, 100);
+    time++;
+    setTimeout(run, 10);
 }
 
 Initiail();
