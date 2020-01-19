57647982b757d9467f4beb4a9de57e00e4d1a56e
skymiku39 <tianya.wakfu@gmail.com>
2020-01-19 21:13:58 +0800

    + 規劃每幀的 routine

---
+++ js/collision.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * 幹你娘
+ * @class Collision
+ */
+class Collision {
+    static sayHello() {
+        console.log("hey");
+    }
+    static sayHello() {
+        console.log("hey");
+    }
+
+}
